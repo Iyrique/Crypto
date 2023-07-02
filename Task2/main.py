9fc338b08e912f66d40f97501824603c796ab51d
remoppou <voronezhskij_n_m@sc.vsu.ru>
2023-07-02 14:08:07 +0300

    t2-very good algorithm

--- Task2/main.py
+++ Task2/main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import numpy as np
+from pyzbar.pyzbar import decode
 
 
 def open_image():
@@ -7,26 +7,43 @@ def open_image():
     return image
 
 
-def creator():
+def creator_qr():
     image = open_image()
     width, height = image.size
-    qr_rb = [[0 for _ in range(height)] for _ in range(width)]
-    qr_rg = [[0 for _ in range(height)] for _ in range(width)]
-    qr_bg = [[0 for _ in range(height)] for _ in range(width)]
+    qr_rb = Image.new("1", (width, height))
+    qr_rg = Image.new("1", (width, height))
+    qr_bg = Image.new("1", (width, height))
+    qr_rb_p = qr_rb.load()
+    qr_rg_p = qr_rg.load()
+    qr_bg_p = qr_bg.load()
     for x in range(width):
         for y in range(height):
             r, g, b = image.getpixel((x, y))
             decoded_r = r & 1
             decoded_g = g & 1
             decoded_b = b & 1
-            qr_rb[x][y] = (decoded_r % 2) * 255 + (decoded_b % 2) * 255
-            qr_rg[x][y] = (decoded_r % 2) * 255 + (decoded_g % 2) * 255
-            qr_bg[x][y] = (decoded_b % 2) * 255 + (decoded_g % 2) * 255
-    Image.fromarray(np.asarray(qr_rb).astype('uint8'), mode="L").save('qr_rb.png')
-    Image.fromarray(np.asarray(qr_rg).astype('uint8'), mode="L").save('qr_rg.png')
-    Image.fromarray(np.asarray(qr_bg).astype('uint8'), mode="L").save('qr_bg.png')
+            qr_rb_p[x, y] = decoded_r ^ decoded_b
+            qr_rg_p[x, y] = decoded_r ^ decoded_g
+            qr_bg_p[x, y] = decoded_b ^ decoded_g
+    qr_rb.save('qr_rb.png')
+    qr_rg.save('qr_rg.png')
+    qr_bg.save('qr_bg.png')
+
+
+def qr_reader(file_name):
+    img = Image.open(file_name)
+    qr_code = decode(img)
+    if qr_code:
+        return qr_code[0].data.decode('utf-8')
+    return None
 
 
 if __name__ == '__main__':
-    creator()
+    creator_qr()
+    if qr_reader("qr_rg.png") is not None:
+        print(qr_reader("qr_rg.png"))
+    elif qr_reader("qr_bg.png") is not None:
+        print(qr_reader("qr_bg.png"))
+    else:
+        print(qr_reader("qr_rb.png"))
 
